0e3c7f8b2d2e95b3e74192bf0881a1421cc530c6
wschadow <wolfgang.schadow@caribou3d.com>
2024-05-12 20:46:42 +0200

    Update hello_mpi.f90

--- hello_mpi.f90
+++ hello_mpi.f90
@@ -7,6 +7,6 @@ program mpi_hello
     call MPI_Comm_size(MPI_COMM_WORLD, size, ierr)
     call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
 
-    print *, 'Hello from rank', rank, 'of', size, 'processors'
+    print *, 'Hello from rank: ', rank, 'of', size, 'processors'
     call MPI_Finalize(ierr)
 end program mpi_hello
